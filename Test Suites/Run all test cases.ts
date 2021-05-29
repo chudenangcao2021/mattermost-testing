375048002db60ac052dbf66541811479a28bfbba
ngthotuan <ngthotuan@gmail.com>
2021-05-29 18:09:50 +0700

    add run all test cases

---
+++ Test Suites/Run all test cases.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d7b766d4-7cf9-4bad-b58a-1a5495c39b00</testSuiteGuid>
+</TestSuiteEntity>
